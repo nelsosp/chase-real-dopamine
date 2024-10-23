dc6a8d8ea5e328522d05427a85c5483cefa46128
nelsosp <124758584+nelsosp@users.noreply.github.com>
2024-10-22 23:07:52 -0400

    functionality fixes

--- src/Components/WebDareFinder.js
+++ src/Components/WebDareFinder.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import './WebDareFinder.css';
 import video from '../assets/WebDareFinder.mp4'
 
-const WebDareFinder = ({ dare, setDare, error, setError, fetchDare }) => {
+const WebDareFinder = ({ dare, error, fetchDare }) => {
     useEffect(() => {
         document.body.classList.add('web-body');
         document.documentElement.classList.add('web-html');
@@ -16,7 +16,7 @@ const WebDareFinder = ({ dare, setDare, error, setError, fetchDare }) => {
     return (
         <div className="web-dare-container">
             <video className="background-video" src={video} muted loop autoPlay></video>
-            <h1>Daily Dare - Web</h1>
+            <h1>Daily Dare</h1>
             <button onClick={fetchDare} className="dare-button">
                 Get Today's Dare
             </button>
