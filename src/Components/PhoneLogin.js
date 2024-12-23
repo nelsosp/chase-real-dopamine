644f69244633ef1d33f82e73c07740ee60e845fa
nelsosp <nelsonsp@mail.gvsu.edu>
2024-12-23 02:59:57 -0500

    Code reformat

--- src/Components/PhoneLogin.js
+++ src/Components/PhoneLogin.js
@@ -1,42 +1,55 @@
-import './PhoneLogin.css';
-import image from '../assets/PhoneLogin.jpg'
+import image from "../assets/PhoneLogin.jpg";
+import "./PhoneLogin.css";
 
-const PhoneLogin = ({ username, setUsername, handleLogin, handleCreateUser, error, successMessage, isLoading, messageIndex, isFadingOut, messages }) => {
-
-    return (
-        <div>
-            {messageIndex !== -1 && (
-            <div className={`message ${isFadingOut ? 'fade-out' : 'fade-in'}`}>
-                {messages[messageIndex]}
-            </div>
-        )}
-        <form className={isLoading ? 'fade-out' : ''}>
-        <div className='phone-page-container'>
-        <img src={image} alt='Background' className='background-image' />
-            <h1>Dopamine Dares</h1>
-            <div className="phone-login-container">
-                <h2>Login</h2>
-                {error && <div className="error">{error}</div>}
-                {successMessage && <div className="success">{successMessage}</div>}
-                    <div>
-                        <label>
-                            Username:
-                            <input
-                                type="text"
-                                value={username}
-                                onChange={(e) => setUsername(e.target.value)}
-                                required
-                            />
-                        </label>
-                    </div>
-                    <button type="button" onClick={handleLogin}>Login</button>
-                    <button type="button" onClick={handleCreateUser}>Create User</button>
-               
-            </div>
+const PhoneLogin = ({
+  username,
+  setUsername,
+  handleLogin,
+  handleCreateUser,
+  error,
+  successMessage,
+  isLoading,
+  messageIndex,
+  isFadingOut,
+  messages,
+}) => {
+  return (
+    <div>
+      {messageIndex !== -1 && (
+        <div className={`message ${isFadingOut ? "fade-out" : "fade-in"}`}>
+          {messages[messageIndex]}
         </div>
-        </form>
+      )}
+      <form className={isLoading ? "fade-out" : ""}>
+        <div className="phone-page-container">
+          <img src={image} alt="Background" className="background-image" />
+          <h1>Dopamine Dares</h1>
+          <div className="phone-login-container">
+            <h2>Login</h2>
+            {error && <div className="error">{error}</div>}
+            {successMessage && <div className="success">{successMessage}</div>}
+            <div>
+              <label>
+                Username:
+                <input
+                  type="text"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  required
+                />
+              </label>
+            </div>
+            <button type="button" onClick={handleLogin}>
+              Login
+            </button>
+            <button type="button" onClick={handleCreateUser}>
+              Create User
+            </button>
+          </div>
         </div>
-    );
+      </form>
+    </div>
+  );
 };
 
 export default PhoneLogin;
