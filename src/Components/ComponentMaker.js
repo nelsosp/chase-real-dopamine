fef92cb27eae42fe761c7ae6715853cf18cfb94f
nelsosp <nelsonsp@mail.gvsu.edu>
2025-01-01 23:45:00 -0500

    Added test page/ create menu nav component

---
+++ src/Components/ComponentMaker.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PhoneMenuBar from "./PhoneMenuBar";
+
+const ComponentMaker = () => {
+  return (
+    <div>
+      <PhoneMenuBar />
+    </div>
+  );
+};
+
+export default ComponentMaker;
--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,7 @@
 // src/Routes.js
 import React from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import ComponentMaker from "./Components/ComponentMaker";
 import DareFinder from "./Components/DareFinder";
 import Login from "./Components/Login";
 
@@ -10,6 +11,7 @@ const AppRoutes = () => {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/dare-finder" element={<DareFinder />} />
+        <Route path="/component-maker" element={<ComponentMaker />} />
         {/* Add more routes as needed */}
       </Routes>
     </Router>
